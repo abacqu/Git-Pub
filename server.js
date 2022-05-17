09825ee90626411f8be32349c1deb227ded9eb22
alec <abacqu0716@gmail.com>
2022-05-17 16:03:25 -0500

    json added

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const drinks = require('./models/drinks');
+
 const app = express();
 
 const port = 3000;
@@ -8,6 +10,9 @@ app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!');
 });
 
+app.get("/drinks", (req, res) => {
+    res.send(drinks);
+});
 
 
 
